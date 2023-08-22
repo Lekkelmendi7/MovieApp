f0611d78a3e042694152e850668d185c05ba5460
Lekkelmendi7 <lek.kelmendi8@gmail.com>
2023-08-22 19:12:48 +0200

    Creating Forms and Validations

---
+++ react-movies/src/genres/CreateGenre.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import Button from "../utils/Button";
+import { Form, Formik } from "formik";
+import * as Yup from 'yup';
+import TextField from "../forms/TextField";
+
+export default function CreateGenre(){
+    return (
+        <>
+            <h3>Create Genre</h3>
+            <Formik initialValues={{
+                name: ''
+            }}
+            
+            onSubmit={async value=> {
+                await new Promise(r => setTimeout(r, 4000));
+                console.log(value);
+            }}
+            validationSchema={Yup.object({
+                name: Yup.string().required('This field is required to fill!').firstLetterUpperCase()
+            })}
+            >
+                {(formikProps) => (
+                    <Form>
+                    <TextField field="name" displayName="Name"/>
+
+                    <Button disabled={formikProps.isSubmitting} type='submit'>Save Changes</Button>
+                    <Link className="btn btn-secondary" to="/genres"> Cancel</Link>
+                </Form>
+                )}
+            </Formik>
+            
+        </>
+    )
+}
---
+++ react-movies/src/movies/LandingPage.tsx
@@ -0,0 +1,46 @@
+import { useEffect, useState } from "react";
+import MoviesList from "./MoviesList";
+import { landingPageDTO } from "./movies.model";
+
+export default function LandingPage(){
+    const [movies, setMovies] = useState<landingPageDTO>({});
+
+    
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setMovies({
+        inTheaters: [
+          {
+          id: 1,
+          title: 'Oppenheimer',
+          poster: 'https://m.media-amazon.com/images/M/MV5BMDBmYTZjNjUtN2M1MS00MTQ2LTk2ODgtNzc2M2QyZGE5NTVjXkEyXkFqcGdeQXVyNzAwMjU2MTY@._V1_.jpg'
+        },
+        {
+          id: 2,
+          title: 'Saving Private Ryan',
+          poster: 'https://m.media-amazon.com/images/M/MV5BZjhkMDM4MWItZTVjOC00ZDRhLThmYTAtM2I5NzBmNmNlMzI1XkEyXkFqcGdeQXVyNDYyMDk5MTU@._V1_FMjpg_UX1000_.jpg'
+        }
+      ],
+      upComingReleases: [
+        {
+          id: 3,
+          title: 'Ted 2',
+          poster: 'https://m.media-amazon.com/images/I/51wY4SwbI5L._AC_UF894,1000_QL80_.jpg'
+        }
+      ]
+      })
+    }, 1000);
+
+    return () => clearTimeout(timerId);
+  });
+
+    return(
+        <>
+         <h3>In Theaters</h3>
+      <MoviesList movies={movies.inTheaters} />
+
+      <h3>Upcoming Releases</h3>
+      <MoviesList movies={movies.upComingReleases} />
+        </>
+    )
+}
