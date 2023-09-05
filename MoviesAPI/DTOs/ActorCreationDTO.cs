190385fadfbcb4971e0eec74082be694ea4e83f0
Lekkelmendi7 <lek.kelmendi8@gmail.com>
2023-09-05 18:11:25 +0200

    Actors CRUD

--- MoviesAPI/DTOs/ActorCreationDTO.cs
+++ MoviesAPI/DTOs/ActorCreationDTO.cs
@@ -1,5 +1,4 @@
 ﻿using System.ComponentModel.DataAnnotations;
-
 namespace MoviesAPI.DTOs
 {
     public class ActorCreationDTO
--- MoviesAPI/Helpers/InAppStorageService.cs
+++ MoviesAPI/Helpers/InAppStorageService.cs
@@ -12,18 +12,20 @@
         }
         public Task DeleteFile(string fileRoute, string containerName)
         {
-            if(string.IsNullOrEmpty(fileRoute))
+            if (string.IsNullOrEmpty(fileRoute))
             {
-                return Task.CompletedTask;  
+                return Task.CompletedTask;
             }
+
             var fileName = Path.GetFileName(fileRoute);
-            var fileDirectory = Path.Combine(env.WebRootPath, containerName , fileName);
+            var fileDirectory = Path.Combine(env.WebRootPath, containerName, fileName);
 
             if (File.Exists(fileDirectory))
             {
                 File.Delete(fileDirectory);
-            } 
-            return Task.CompletedTask;  
+            }
+
+            return Task.CompletedTask;
         }
 
         public async Task<string> EditFile(string containerName, IFormFile file, string fileRoute)
